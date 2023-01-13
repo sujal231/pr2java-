ec5562dfc5bc7f2c3e16c8e3924f167584eee578
sujal231 <115794420+sujal231@users.noreply.github.com>
2023-01-13 17:54:24 +0530

    Update pr2_1.java

--- pr2_1.java
+++ pr2_1.java
@@ -2,21 +2,30 @@ class pr{
     public static void main(String[] args) 
     {
         Book z= new Book();
-        Book z1= new Book(123,"Ramanujans Notebooks","Ramanujan");
+        Book z1= new Book(123,"ramanujan lost Notebook","Ramanujan");
+         z1.print();
     }
 }
 
 class Book
 {
+    int book_id;
+    String title;
+    String author;
     Book()
     {
         System.out.println("Book informetion:- ");
     }
-    Book(int book_id,String title,String author)
+    Book(int bid,String btitle , String bauthor)
     {
+        book_id=bid;
+        title = btitle;
+        author= bauthor;
+    }
+    void print(){
         System.out.println("Book id= "+book_id);
         System.out.println("Book Title= "+title);
         System.out.println("Book Author= "+author);
-    }
-
+        }
 }
+
